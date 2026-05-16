56d34a46c4ddffc03cd5d5dd9e124f64f0a4102a
prashant-bbipl <prashantbbipl@gmail.com>
2026-05-16 22:57:05 +0530

    Update App.jsx

--- Frontend/form-frontend/src/App.jsx
+++ Frontend/form-frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route, useParams } from 'react-router-dom';
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'http://34.239.45.44:5000/api';
 
 function FormBuilder() {
   const [title, setTitle] = useState('');
